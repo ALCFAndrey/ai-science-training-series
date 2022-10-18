8dfd4e23efddde066d506d2f2fc8bef93bb9e574
ALCFAndrey <theandrewsha@gmail.com>
2022-10-18 18:17:49 -0500

    Homework 5

--- 05_dataPipelines/train_resnet34_thetagpu.sh
+++ 05_dataPipelines/train_resnet34_thetagpu.sh
@@ -10,7 +10,20 @@ module load conda/2022-07-01
 conda activate
 
 # You have to point this to YOUR local copy of ai-science-training-series
-cd /home/cadams/ThetaGPU/ai-science-training-series/04_modern_neural_networks
+cd /home/amakhanov/ai-science-training-series/05_dataPipelines
 
 export TF_XLA_FLAGS="--tf_xla_auto_jit=2"
-python train_resnet34.py
+python train_net_128_1.py
+python train_net_128_2.py
+python train_net_128_4.py
+python train_net_128_8.py
+
+python train_net_64_1.py
+python train_net_64_2.py
+python train_net_64_4.py
+python train_net_64_8.py
+
+python train_net_32_1.py
+python train_net_32_2.py
+python train_net_32_4.py
+python train_net_32_8.py
